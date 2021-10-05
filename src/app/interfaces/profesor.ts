719bc897694a93f8ab232452d0d7f84783a89e3a
Gabriel Fuentes <nunkmeconecto@gmail.com>
2021-10-05 09:44:35 -0300

    Se agrega Interfaz profesor

---
+++ src/app/interfaces/profesor.ts
@@ -0,0 +1,11 @@
+export interface Profesor {
+  id: number;
+  nombre: string;
+  email: string;
+  apellido?: string;
+  edad?: number;
+  grado_academico?: string;
+  asignatura?: string;
+  disponibilidad?: string;
+  anos_experiencia?: string;
+}
