e57a1a9bd8a163e179f19b5d61d943b1f6550a76
Gabriel Fuentes <nunkmeconecto@gmail.com>
2021-10-05 16:26:09 -0300

    Cambios menores que no han modificado el func

--- src/app/services/profesor.service.ts
+++ src/app/services/profesor.service.ts
@@ -3,24 +3,27 @@ import { HttpClient } from '@angular/common/http';
 import { Profesor } from '../interfaces/profesor.interface';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ProfesorService {
   private url: string;
   profesor: Profesor[] = [];
 
   constructor(private http: HttpClient) {
-    this.url = "http://localhost:8080/api";
+    this.url = 'http://localhost:8080/api';
     this.cargarProfesores();
   }
 
   public cargarProfesores() {
-    this.http.get(this.url+"/profesores").subscribe( (response: Profesor | any) => {
-      this.profesor = response.productos;
-    });
+    this.http
+      .get(this.url + '/profesores')
+      .subscribe((response: Profesor | any) => {
+        this.profesor = response.productos;
+        console.log(this.profesor);
+      });
   }
 
   public save(profesor: Profesor) {
-    return this.http.post<Profesor>(this.url+"agregar_profesor", profesor);
+    return this.http.post<Profesor>(this.url + 'agregar_profesor', profesor);
   }
 }
